0023bf8e8c5a8f27c1b3a06b7223ffa4637451c5
imagine243 <imagine243@gmail.com>
2019-08-25 00:12:34 -0700

    add mac install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+# xcode unity
+
+# homebrew 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+brew install git macvim mono cmake
+
+brew cask install google-chrome sogouinput rider iterm2 visual-studio-code
+
+ssh-keygen
+
+# vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
