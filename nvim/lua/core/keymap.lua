15bf9b3903e748f0e2bbd00665a1f5fe55a4deb5
chao <imagine243@gmail.com>
2022-10-11 17:01:05 +0800

    ------

--- nvim/lua/core/keymap.lua
+++ nvim/lua/core/keymap.lua
@@ -10,8 +10,8 @@ local map_cu = function(mode, keymap, cmd, opt)
 	map(mode, keymap, real_cmd, opt)
 end
 
-map("n", "<C-j>", "4j", opt)
-map("n", "<C-k>", "4k", opt)
+-- map("n", "<C-j>", "4j", opt)
+-- map("n", "<C-k>", "4k", opt)
 map("i", "<C-h>", "<ESC>I", opt)
 map("i", "<C-l>", "<ESC>A", opt)
 
@@ -80,7 +80,7 @@ map_cu("n", "<leader>fe", "NvimTreeToggle", opt)
 
 -- telescope
 map_cu("n", "<leader>ff", "Telescope find_files theme=ivy", opt)
-map_cu("n", "<leader>fr", "Telescope oldfiles theme=ivy", opt)
+map_cu("n", "<leader>fo", "Telescope oldfiles theme=ivy", opt)
 map_cu("n", "<leader>fp", "Telescope projects theme=ivy", opt)
 map_cu("n", "<leader>fb", "Telescope file_browser theme=ivy", opt)
 map_cu("n", "<leader>ss", "Telescope grep_string theme=ivy", opt)
