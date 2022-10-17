28f956f70f3ff827160cd3f043ac7780d15de3c9
chao <imagine243@gmail.com>
2022-10-17 18:41:58 +0800

    edit

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -59,7 +59,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 nvim_lsp.sumneko_lua.setup({
 	on_attach = on_attach,
