bd025f85c1d48d23aa85906f45e4dff56921d16d
chao <imagine243@gmail.com>
2022-10-18 22:56:05 +0800

    ---

--- nvim/lua/plugin/nvim_comment.lua
+++ nvim/lua/plugin/nvim_comment.lua
@@ -1,3 +1,1 @@
-require('nvim_comment').setup(
-    {line_mapping = "<leader>cc", operator_mapping = "<leader>c"}
-)
+require("nvim_comment").setup({ line_mapping = "<leader>cc", operator_mapping = "<leader>c" })
