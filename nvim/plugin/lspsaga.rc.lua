63fb8e99d2e57aa1ed58a6f0a7f2c55257e72530
chao <imagine243@gmail.com>
2022-10-18 13:57:16 +0800

    ---

--- nvim/plugin/lspsaga.rc.lua
+++ nvim/plugin/lspsaga.rc.lua
@@ -11,9 +11,9 @@ saga.init_lsp_saga({
 
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
-keymap("n", "jn", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-keymap("n", "jp", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-keymap("n", "jc", "<Cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+keymap("n", "gn", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+keymap("n", "gp", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+keymap("n", "gc", "<Cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
 keymap("n", "gh", "<Cmd>Lspsaga hover_doc<CR>", opts)
 keymap("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
 -- keymap("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
